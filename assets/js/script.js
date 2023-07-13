5f3c22e0ffbb67afbe35589b5703f086f27e2cdc
juanes G <jgalvez98@gmail.com>
2023-07-13 18:12:30 -0400

    javascript show questions and  choices/buttons css styles added

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,45 +3,46 @@ var introSectionEl= document.getElementById("intro-section")
 var questionSectionEl=document.getElementById("question-section")
 var titleEl=document.getElementById("title")
 var timerEl=document.getElementById("timer")
-var choicesEl = document.querySelectorAll("choices")
-var showscore = document.getElementById("score")
+var choicesEl= document.querySelectorAll("choices")
+/* var showscore = document.getElementById("score") */
 var questionIndex=0
 var questionsArray=[
     {
         title:"Commonly used data types DO NOT include:",
-        choices:["1.strings, 2.booleans, 3.alerts, 4.numbers"],
-        correctanswer: "3.alerts"
+        choices:["1.strings", "2.booleans", "3.alerts", "4.numbers"],
+        answer: "3.alerts"
     },
     {
         title:"The condition in an if / else statement is enclosed within ____.",
-        choices:["1.quotes, 2.curly brackets, 3.parentesis, 4.square brackets"],
-        correctanswer: "2.curly brackets"
+        choices:["1.quotes", "2.curly brackets", "3.parentesis", "4.square brackets"],
+        answer: "2.curly brackets"
     },
     {
         title:"Arrays in javascript can be used to store _____.",
-        choices:["1.numbers and strings, 2.other arrays, 3.booleans, 4.all of the above"],
-        correctanswer: "4.all of the above"
+        choices:["1.numbers and strings", "2.other arrays", "3.booleans", "4.all of the above"],
+        answer: "4.all of the above"
     },
     {
         title:"String values must be enclosed within ____ when being assigned to variables",
-        choices:["1.commas, 2.curly brackets, 3.quotes, 4.parenthesis"],
-        correctanswer: "1.commas"
+        choices:["1.commas", "2.curly", "brackets", "3.quotes", "4.parenthesis"],
+        answer: "1.commas"
     },
     {
         title:"A very useful tool used during development and debugging for printing content to the debugger is:",
-        choices:["1.javascript, 2.terminal/bash, 3.for loops, 4.console log"],
-        correctanswer: "3.for loops"
+        choices:["1.javascript", "2.terminal/bash", "3.for loops", "4.console log"],
+        answer: "3.for loops"
     },
-];
+]
 
 var timeLeft=questionsArray.length* 15
 
 /*
 
 1. hide intro (done)
-2. start timer
-3. show questions
+2. start timer (done)
+3. show questions (done)
 4. structure to store questions and choices
+5. structure to show score/enter intials after finishing questions
 
 */
 
@@ -72,6 +73,7 @@ function showQuestions(){
     choicesEl[2].textContent=questionsArray[questionIndex].choices[2]
 
     choicesEl[3].textContent=questionsArray[questionIndex].choices[3]
+    
 
 
 }
@@ -84,10 +86,13 @@ function nextQuestion (event){
   }
 }
 
-function showscore(){
+/*function showscore(event){
+    var currentElement= event.target
+    if(currentElement.matches("")){
 
+    }
 }
-
+*/
 
 startBtn.addEventListener("click", startQuiz)
 
