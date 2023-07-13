56679e0d99e3857ce35c347a4005c3180dd2c872
juanes G <jgalvez98@gmail.com>
2023-07-12 21:17:57 -0400

    html bugs fixed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,37 +1,38 @@
-var startBtn = document.getElementById
+var startBtn= document.getElementById("start-btn")
 var introSectionEl= document.getElementById("intro-section")
 var questionSectionEl=document.getElementById("question-section")
 var titleEl=document.getElementById("title")
 var timerEl=document.getElementById("timer")
 var choicesEl = document.querySelectorAll("choices")
-var questionindex=0
+var showscore = document.getElementById("score")
+var questionIndex=0
 var questionsArray=[
     {
-        title:"question 1",
-        choices:["choice1, c2, c3, c4"],
-        answer: "c2"
+        title:"Commonly used data types DO NOT include:",
+        choices:["1.strings, 2.booleans, 3.alerts, 4.numbers"],
+        correctanswer: "3.alerts"
     },
     {
-        title:"question 2",
-        choices:["choice1, c2, c3, c4"],
-        answer: "c2"
+        title:"The condition in an if / else statement is enclosed within ____.",
+        choices:["1.quotes, 2.curly brackets, 3.parentesis, 4.square brackets"],
+        correctanswer: "2.curly brackets"
     },
     {
-        title:"question 3",
-        choices:["choice1, c2, c3, c4"],
-        answer: "c2"
+        title:"Arrays in javascript can be used to store _____.",
+        choices:["1.numbers and strings, 2.other arrays, 3.booleans, 4.all of the above"],
+        correctanswer: "4.all of the above"
     },
     {
-        title:"question 4",
-        choices:["choice1, c2, c3, c4"],
-        answer: "c2"
+        title:"String values must be enclosed within ____ when being assigned to variables",
+        choices:["1.commas, 2.curly brackets, 3.quotes, 4.parenthesis"],
+        correctanswer: "1.commas"
     },
     {
-        title:"question 5",
-        choices:["choice1, c2, c3, c4"],
-        answer: "c2"
-    }
-]
+        title:"A very useful tool used during development and debugging for printing content to the debugger is:",
+        choices:["1.javascript, 2.terminal/bash, 3.for loops, 4.console log"],
+        correctanswer: "3.for loops"
+    },
+];
 
 var timeLeft=questionsArray.length* 15
 
@@ -47,10 +48,10 @@ var timeLeft=questionsArray.length* 15
 var setIntervalId=0;
 
 function startQuiz(){
-    introSectionEl.classList.add("hide")
-    questionSectionEl.removeAttribute("class")
-    setIntervalId=setInterval(countDown,1000)
-
+  introSectionEl.setAttribute("class","hide")
+  questionSectionEl.removeAttribute("class")
+  setIntervalId=setInterval(countDown,1000)
+  showQuestions()
 }
 
 function countDown(){
@@ -61,17 +62,16 @@ function countDown(){
 }
 
 
+function showQuestions(){
+    titleEl.textContent=questionsArray[questionIndex].title
 
-function showQuestions (){
-    titleEl.textContent=questionsArray[questionindex].title
+    choicesEl[0].textContent=questionsArray[questionIndex].choices[0]
 
-    choicesEl[0].textContent=questionsArray[questionindex].choices[0]
+    choicesEl[1].textContent=questionsArray[questionIndex].choices[1]
 
-    choicesEl[1].textContent=questionsArray[questionindex].choices[1]
+    choicesEl[2].textContent=questionsArray[questionIndex].choices[2]
 
-    choicesEl[2].textContent=questionsArray[questionindex].choices[2]
-
-    choicesEl[3].textContent=questionsArray[questionindex].choices[3]
+    choicesEl[3].textContent=questionsArray[questionIndex].choices[3]
 
 
 }
@@ -84,7 +84,11 @@ function nextQuestion (event){
   }
 }
 
+function showscore(){
 
-startbtn.addEventListener("click", startQuiz)
+}
 
-questionSectionEl.addEventListener("click",nextQuestion)
+
+startBtn.addEventListener("click", startQuiz)
+
+questionSectionEl.addEventListener("click",nextQuestion )
